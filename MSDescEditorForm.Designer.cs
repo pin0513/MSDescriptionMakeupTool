d08ae46e406fd65aa19fb1774f7ff003b0ea66ce
保羅 <pin0513@gmail.com>
2021-06-06 20:02:45 +0800

    first commit

---
+++ MSDescEditorForm.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace MsDescriptionMakeUpTool
+{
+    partial class MSDescEditorForm
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnConnectDB = new System.Windows.Forms.Button();
+            this.tbConnStr = new System.Windows.Forms.TextBox();
+            this.cbDBList = new System.Windows.Forms.ComboBox();
+            this.tbExportScript = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.tbMS_DescriptionForTable = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnExport = new System.Windows.Forms.Button();
+            this.btnSyncDB = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 100);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 27;
+            this.dataGridView1.Size = new System.Drawing.Size(1047, 170);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellEndEdit);
+            // 
+            // btnConnectDB
+            // 
+            this.btnConnectDB.Location = new System.Drawing.Point(22, 10);
+            this.btnConnectDB.Margin = new System.Windows.Forms.Padding(2);
+            this.btnConnectDB.Name = "btnConnectDB";
+            this.btnConnectDB.Size = new System.Drawing.Size(56, 22);
+            this.btnConnectDB.TabIndex = 1;
+            this.btnConnectDB.Text = "連線";
+            this.btnConnectDB.UseVisualStyleBackColor = true;
+            this.btnConnectDB.Click += new System.EventHandler(this.btnConnectDB_Click);
+            // 
+            // tbConnStr
+            // 
+            this.tbConnStr.Location = new System.Drawing.Point(84, 10);
+            this.tbConnStr.Margin = new System.Windows.Forms.Padding(2);
+            this.tbConnStr.Name = "tbConnStr";
+            this.tbConnStr.Size = new System.Drawing.Size(479, 22);
+            this.tbConnStr.TabIndex = 2;
+            // 
+            // cbDBList
+            // 
+            this.cbDBList.FormattingEnabled = true;
+            this.cbDBList.Location = new System.Drawing.Point(567, 10);
+            this.cbDBList.Margin = new System.Windows.Forms.Padding(2);
+            this.cbDBList.Name = "cbDBList";
+            this.cbDBList.Size = new System.Drawing.Size(502, 20);
+            this.cbDBList.TabIndex = 3;
+            this.cbDBList.SelectedIndexChanged += new System.EventHandler(this.cbDBList_SelectedIndexChanged);
+            // 
+            // tbExportScript
+            // 
+            this.tbExportScript.Location = new System.Drawing.Point(22, 303);
+            this.tbExportScript.Margin = new System.Windows.Forms.Padding(2);
+            this.tbExportScript.Multiline = true;
+            this.tbExportScript.Name = "tbExportScript";
+            this.tbExportScript.ReadOnly = true;
+            this.tbExportScript.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.tbExportScript.Size = new System.Drawing.Size(1048, 195);
+            this.tbExportScript.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 46);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Table描述";
+            // 
+            // tbMS_DescriptionForTable
+            // 
+            this.tbMS_DescriptionForTable.Location = new System.Drawing.Point(84, 43);
+            this.tbMS_DescriptionForTable.Margin = new System.Windows.Forms.Padding(2);
+            this.tbMS_DescriptionForTable.Name = "tbMS_DescriptionForTable";
+            this.tbMS_DescriptionForTable.Size = new System.Drawing.Size(258, 22);
+            this.tbMS_DescriptionForTable.TabIndex = 6;
+            this.tbMS_DescriptionForTable.TextChanged += new System.EventHandler(this.tbMS_DescriptionForTable_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 78);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(71, 12);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Columns描述";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(994, 73);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "儲存";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnExport
+            // 
+            this.btnExport.Location = new System.Drawing.Point(995, 275);
+            this.btnExport.Name = "btnExport";
+            this.btnExport.Size = new System.Drawing.Size(75, 23);
+            this.btnExport.TabIndex = 8;
+            this.btnExport.Text = "匯出";
+            this.btnExport.UseVisualStyleBackColor = true;
+            this.btnExport.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // btnSyncDB
+            // 
+            this.btnSyncDB.Location = new System.Drawing.Point(22, 275);
+            this.btnSyncDB.Name = "btnSyncDB";
+            this.btnSyncDB.Size = new System.Drawing.Size(75, 23);
+            this.btnSyncDB.TabIndex = 9;
+            this.btnSyncDB.Text = "資料庫同步";
+            this.btnSyncDB.UseVisualStyleBackColor = true;
+            this.btnSyncDB.Click += new System.EventHandler(this.btnSyncDB_Click);
+            // 
+            // MSDescEditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1102, 521);
+            this.Controls.Add(this.btnSyncDB);
+            this.Controls.Add(this.btnExport);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbMS_DescriptionForTable);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbExportScript);
+            this.Controls.Add(this.cbDBList);
+            this.Controls.Add(this.tbConnStr);
+            this.Controls.Add(this.btnConnectDB);
+            this.Controls.Add(this.dataGridView1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.Name = "MSDescEditorForm";
+            this.Text = "MSDesc修補工具";
+            this.Load += new System.EventHandler(this.MSDescEditorForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnConnectDB;
+        private System.Windows.Forms.TextBox tbConnStr;
+        private System.Windows.Forms.ComboBox cbDBList;
+        private System.Windows.Forms.TextBox tbExportScript;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox tbMS_DescriptionForTable;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnExport;
+        private System.Windows.Forms.Button btnSyncDB;
+    }
+}
+
---
+++ MSDescEditorForm.cs
@@ -0,0 +1,414 @@
+﻿using Dapper;
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DeployWizardUtils;
+
+namespace MsDescriptionMakeUpTool
+{
+    public partial class MSDescEditorForm : Form
+    {
+        public MSDescEditorForm()
+        {
+            InitializeComponent();
+        }
+
+        private string _connectionString = string.Empty;
+
+        private ConcurrentDictionary<string, TableDesciption> _collections;
+
+
+
+        private void btnConnectDB_Click(object sender, EventArgs e)
+        {
+
+
+            _connectionString = tbConnStr.Text;
+            var scsb = new SqlConnectionStringBuilder(_connectionString);
+
+            DeployWizardUtils.SqlHelper.CheckSQLConnection(_connectionString, true);
+
+            if (!string.IsNullOrEmpty(scsb.InitialCatalog))
+            {
+                loadTableList(scsb.InitialCatalog);
+            }
+            else
+            {
+                MessageBox.Show("連線字串必須包含資料庫");
+                return;
+            }
+        }
+
+
+        private void loadTableList(string dbName)
+        {
+            var scriptForListDB =
+                @"SELECT TABLE_NAME FROM {0}.INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' Order By 1 ";
+            //ForTest 
+            //@"SELECT top 2 TABLE_NAME FROM {0}.INFORMATION_SCHEMA.TABLES WHERE TABLE_TYPE = 'BASE TABLE' Order By 1 ";
+
+            scriptForListDB = string.Format(scriptForListDB, dbName);
+
+            var cn = new SqlConnection(_connectionString);
+
+            var table_list = cn.Query<string>(scriptForListDB).ToList();
+
+            var tableListCollections = new ConcurrentDictionary<string, string>();
+            _collections = new ConcurrentDictionary<string, TableDesciption>();
+
+            Parallel.ForEach(table_list.ToArray(), (tableName) =>
+            {
+                try
+                {
+                    var cn_inner = new SqlConnection(_connectionString);
+
+                    _collections.TryAdd(tableName,
+                                        new TableDesciption() { Name = tableName, Columns = new List<ColumnDesciption>() });
+
+                    var scriptForListTableColumn =
+                        @"SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'{0}'";
+                    scriptForListTableColumn = string.Format(scriptForListTableColumn, tableName);
+
+                    var columns_list = cn_inner.Query<string>(scriptForListTableColumn).ToList();
+
+                    var checkMSDescForTable =
+                        "SELECT value FROM SYS.EXTENDED_PROPERTIES WHERE [major_id] = OBJECT_ID('{0}') AND [name] = N'MS_Description' AND [minor_id] = 0";
+
+                    var columnSumCount = 0;
+                    foreach (var colName in columns_list)
+                    {
+                        var checkMSDescForColumn =
+                            "SELECT value FROM SYS.EXTENDED_PROPERTIES WHERE [major_id] = OBJECT_ID('{0}') AND [name] = N'MS_Description' AND [minor_id] = (SELECT [column_id] FROM SYS.COLUMNS WHERE [name] = '{1}' AND [object_id] = OBJECT_ID('{0}'))";
+                        checkMSDescForColumn = string.Format(checkMSDescForColumn, tableName, colName);
+
+                        var ColumnExistMSDescResult = cn_inner.Query<string>(checkMSDescForColumn).ToList();
+
+                        var ColumnExistMSDesc = ColumnExistMSDescResult.FirstOrDefault();
+                        var isExist = false;
+                        if (ColumnExistMSDesc != null && ColumnExistMSDesc.ToString().Trim() != "")
+                        {
+                            columnSumCount += 1;
+                            isExist = true;
+                        }
+
+                        _collections[tableName].Columns.Add(new ColumnDesciption()
+                        { Name = colName, MS_Description = ColumnExistMSDesc, isExist = isExist });
+                    }
+
+
+                    checkMSDescForTable = string.Format(checkMSDescForTable, tableName);
+                    var tableExistMSDescResult = cn_inner.Query<string>(checkMSDescForTable).ToList();
+
+                    var tableExistMSDesc = tableExistMSDescResult.FirstOrDefault();
+
+                    var tableMSDescCount = 0;
+                    if (tableExistMSDesc != null && tableExistMSDesc.ToString().Trim() != "")
+                    {
+                        _collections[tableName].isExist = true;
+                        _collections[tableName].MS_Description = tableExistMSDesc.ToString().Trim();
+                        tableMSDescCount++;
+                    }
+
+                    var DisplayTableName = string.Format("{0} ({1}) 欄位註釋 - 已:{2}/總:{3}", tableName, tableExistMSDesc,
+                        columnSumCount, columns_list.Count);
+
+                    if (!tableListCollections.ContainsKey(DisplayTableName))
+                    {
+                        tableListCollections.TryAdd(DisplayTableName, tableName);
+                    }
+                    else
+                    {
+                        tableListCollections.TryAdd(DisplayTableName + "/重覆 " + DateTime.Now.Ticks, tableName);
+                    }
+                }
+                catch (Exception e)
+                {
+
+                    MessageBox.Show(string.Format("load table error, table:{0}, msg:{1}", tableName, e.Message));
+                }
+
+            });
+
+            var orderedCollections = new SortedList<string, string>();
+            var orderdkey = tableListCollections.Keys.ToArray().OrderBy(a => a);
+
+            foreach (var key in orderdkey)
+            {
+                orderedCollections.Add(key, tableListCollections[key]); 
+            }
+
+            cbDBList.DisplayMember = "Key";
+            cbDBList.ValueMember = "Value";
+            cbDBList.DataSource = orderedCollections.ToList();
+        }
+
+        private void cbDBList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (SqlHelper.CheckSQLConnection(_connectionString, false))
+                {
+                    if (cbDBList.SelectedValue == null) return;
+                    var tableName = cbDBList.SelectedValue.ToString();
+
+                    tbMS_DescriptionForTable.Text = _collections[tableName].MS_Description;
+
+                    dataGridView1.DataSource = _collections[tableName].Columns;
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("發生錯誤, Msg:" + exception.Message);
+            }
+
+        }
+
+        private void btnSyncDB_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("尚未支援");
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (SqlHelper.CheckSQLConnection(_connectionString, false))
+                {
+                    //統一處理異動
+
+                    var updateCount = 0;
+                    var modifiedTables = _collections.Where(a => a.Value.modify).ToList();
+                    var commandList = new List<string>();
+
+                    List<object> table_paras = new List<object>();
+
+                    if (modifiedTables.Count > 0)
+                    {
+                        var updateScript = getTableMSDescriptionUpdateScript();
+                        foreach (var tableObj in modifiedTables)
+                        {
+                            table_paras.Add(new { table = tableObj.Value.Name, desc = tableObj.Value.MS_Description });
+                            updateCount++;
+                        }
+
+                        if (table_paras.Count > 0)
+                        {
+                            var cn = new SqlConnection(_connectionString);
+
+                            cn.Execute(updateScript, table_paras);
+                        }
+
+                        var updateScript1 = getColumnMSDescriptionUpdateScript();
+                        List<object> column_paras = new List<object>();
+                        foreach (var tables in _collections)
+                        {
+                            if (tables.Value.Columns.Any(a => a.modify))
+                            {
+                                foreach (var columnObj in tables.Value.Columns.Where(a => a.modify).ToList())
+                                {
+                                    column_paras.Add(new { table = tables.Key, column = columnObj.Name, desc = columnObj.MS_Description });
+                                    updateCount++;
+                                }
+                            }
+                        }
+
+                        if (column_paras.Count > 0)
+                        {
+                            var cn = new SqlConnection(_connectionString);
+
+                            cn.Execute(updateScript1, column_paras);
+                        }
+                    }
+
+                    var connStrBuilder = new SqlConnectionStringBuilder(_connectionString);
+
+                    if (updateCount > 0)
+                    {
+                        loadTableList(connStrBuilder.InitialCatalog);
+                    }
+                    else
+                    {
+                        MessageBox.Show("未發現任何異動!");
+                    }
+                }
+
+
+                MessageBox.Show("儲存成功，確定後，將重新刷新，請稍後!!");
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("發生錯誤, Msg:" + exception.Message);
+            }
+        }
+
+        private static string getColumnMSDescriptionUpdateScript()
+        {
+            var updateScript1 =
+                @"
+IF NOT EXISTS (SELECT NULL FROM SYS.EXTENDED_PROPERTIES WHERE [major_id] = OBJECT_ID(@table) AND [name] = N'MS_Description' AND [minor_id] = (SELECT [column_id] FROM SYS.COLUMNS WHERE [name] =@column AND [object_id] = OBJECT_ID(@table)))
+BEGIN
+    EXECUTE sp_addextendedproperty @name = N'MS_Description', @value = @desc, @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = @table, @level2type = N'COLUMN', @level2name = @column;;
+END
+ELSE
+BEGIN
+    EXECUTE sp_updateextendedproperty @name = N'MS_Description', @value = @desc, @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = @table, @level2type = N'COLUMN', @level2name = @column;
+END
+";
+            return updateScript1;
+        }
+
+        private static string getTableMSDescriptionUpdateScript()
+        {
+            return @"
+IF NOT EXISTS (SELECT NULL FROM SYS.EXTENDED_PROPERTIES WHERE [major_id] = OBJECT_ID(@table) AND [name] = N'MS_Description' AND [minor_id] = 0)
+BEGIN 
+    EXECUTE sp_addextendedproperty @name = N'MS_Description', @value =@desc, @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = @table;
+END
+ELSE
+BEGIN 
+    EXECUTE sp_updateextendedproperty @name = N'MS_Description', @value =@desc, @level0type = N'SCHEMA', @level0name = N'dbo', @level1type = N'TABLE', @level1name = @table;
+END
+     ";
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (SqlHelper.CheckSQLConnection(_connectionString, false))
+                {
+                    if (cbDBList.SelectedValue == null) return;
+                    var tableName = cbDBList.SelectedValue.ToString();
+
+                    if (!string.IsNullOrEmpty(tbMS_DescriptionForTable.Text))
+                    {
+                        _collections[tableName].MS_Description = tbMS_DescriptionForTable.Text;
+                        _collections[tableName].modify = true;
+                    }
+
+                    var idx = 0;
+                    foreach (var tableObj in _collections)
+                    {
+                        if (string.IsNullOrEmpty(tableObj.Value.MS_Description)) continue;
+                        var updateScript = getTableMSDescriptionUpdateScript();
+
+                        updateScript = "declare @table nvarchar(100)='" + tableName + "';declare @desc nvarchar(100) = '" + tableObj.Value.MS_Description + "';\r\n " + updateScript;
+                        updateScript = updateScript.Replace("@table", "@table" + idx);
+                        updateScript = updateScript.Replace("@desc", "@desc" + idx);
+                        tbExportScript.Text += updateScript;
+
+                        tbExportScript.Text += "\r\n GO \r\n";
+
+                        idx++;
+                    }
+
+                    foreach (var tables in _collections)
+                    {
+                        foreach (var column in tables.Value.Columns)
+                        {
+                            if (string.IsNullOrEmpty(column.MS_Description)) continue;
+
+                            var updateScript = getColumnMSDescriptionUpdateScript();
+
+                            updateScript = "declare @table nvarchar(100)='" + tables.Value.Name + "';declare @column nvarchar(100) = '" + column.Name + "'; declare @desc nvarchar(100) = '" + column.MS_Description + "';\r\n " + updateScript;
+                            updateScript = updateScript.Replace("@table", "@table" + idx);
+                            updateScript = updateScript.Replace("@column", "@column" + idx);
+                            updateScript = updateScript.Replace("@desc", "@desc" + idx);
+                            tbExportScript.Text += updateScript;
+
+                            tbExportScript.Text += "\r\n GO \r\n";
+
+                            idx++;
+                        }
+                    }
+                    var connStrBuilder = new SqlConnectionStringBuilder(_connectionString);
+                }
+
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show("發生錯誤, Msg:" + exception.Message);
+            }
+        }
+
+        private void dataGridView1_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (SqlHelper.CheckSQLConnection(_connectionString, false))
+            {
+                if (cbDBList.SelectedValue == null) return;
+                var tableName = cbDBList.SelectedValue.ToString();
+
+                foreach (DataGridViewRow row in dataGridView1.Rows)
+                {
+                    var colName = row.Cells["Name"].Value;
+
+                    if (row.Cells["MS_Description"].Value != null)
+                    {
+                        var colMS_Description = row.Cells["MS_Description"].Value.ToString();
+
+                        var ColumnObject = _collections[tableName].Columns.FirstOrDefault(a => a.Name == colName);
+                        if (ColumnObject != null)
+                        {
+                            ColumnObject.MS_Description = colMS_Description;
+                            ColumnObject.modify = true;
+                        }
+                    }
+                }
+            }
+        }
+
+        private void tbMS_DescriptionForTable_TextChanged(object sender, EventArgs e)
+        {
+            if (SqlHelper.CheckSQLConnection(_connectionString, false))
+            {
+                if (cbDBList.SelectedValue == null) return;
+                var tableName = cbDBList.SelectedValue.ToString();
+
+                if (!string.IsNullOrEmpty(tbMS_DescriptionForTable.Text))
+                {
+                    _collections[tableName].MS_Description = tbMS_DescriptionForTable.Text;
+                    _collections[tableName].modify = true;
+                }
+            }
+
+        }
+
+        private void MSDescEditorForm_Load(object sender, EventArgs e)
+        {
+            if (ConfigurationManager.AppSettings.Get("defaultConn") != null)
+                tbConnStr.Text = ConfigurationManager.AppSettings.Get("defaultConn");
+        }
+    }
+
+    public class TableDesciption
+    {
+        public string Name { get; set; }
+        public string MS_Description { get; set; }
+
+        public List<ColumnDesciption> Columns { get; set; }
+
+        public bool isExist = false;
+        public bool modify = false;
+    }
+
+
+    public class ColumnDesciption
+    {
+        public string Name { get; set; }
+        public string MS_Description { get; set; }
+
+        public bool isExist = false;
+
+        public bool modify = false;
+    }
+
+}
